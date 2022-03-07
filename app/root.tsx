857049d0691ecd8a59bc84c7b7fc29e03ffd3994
Mahesh Rai <mahesh.rai@aps.com>
2022-03-07 06:52:19 -0700

    Added error handling

--- app/root.tsx
+++ app/root.tsx
@@ -1,37 +1,74 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-  LinksFunction,
-} from "remix";
-import type { MetaFunction } from "remix";
+import { Links, LiveReload, Meta, Outlet, Scripts, useCatch } from "remix";
+import type { LinksFunction, MetaFunction } from "remix";
 
 import styles from "./tailwind.css";
 
 export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export const meta: MetaFunction = () => {
-  return { title: "Keep Track App" };
+  const description = `Keep track of anything`;
+  return {
+    description,
+    keywords: "Keep Track App",
+  };
 };
 
-export default function App() {
+function Document({
+  children,
+  title,
+}: {
+  children: React.ReactNode;
+  title?: string;
+}) {
   return (
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
+        {title ? <title>{title}</title> : null}
         <Links />
       </head>
       <body>
-        <Outlet />
-        <ScrollRestoration />
+        {children}
         <Scripts />
         <LiveReload />
       </body>
     </html>
+  );
+}
+
+export default function App() {
+  // throw new Error("Not implemented");
+  return (
+    <Document>
+      <Outlet />
+    </Document>
+  );
+}
+
+export function CatchBoundary() {
+  const caught = useCatch();
+
+  return (
+    <Document title={`${caught.status} ${caught.statusText}`}>
+      <div className="bg-red-300">
+        <h1>
+          {caught.status} {caught.statusText}
+        </h1>
+      </div>
+    </Document>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error);
+
+  return (
+    <Document title="Uh-oh!">
+      <div className="bg-red-300">
+        <h1>App Error</h1>
+        <pre>{error.message}</pre>
+      </div>
+    </Document>
   );
 }
